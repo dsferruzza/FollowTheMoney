a23094b80e6f95562748417659e5eb296d07cbad
David Sferruzza <david.sferruzza@gmail.com>
2014-05-01 15:44:21 +0200

    Add expense creation form

--- app/controllers/Expense.scala
+++ app/controllers/Expense.scala
@@ -2,11 +2,31 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Expense extends Controller {
+
+	case class ExpenseData(date: String, id_category: Long, description: Option[String], amount: BigDecimal)
+
+	val expenseForm = Form(
+		mapping(
+			"date" -> text,
+			"id_category" -> longNumber,
+			"description" -> optional(text),
+			"amount" -> bigDecimal
+		)(ExpenseData.apply)(ExpenseData.unapply)
+	)
 
 	def index = Action {
 		Ok(views.html.expense(models.Expense.getAllWithCategory))
 	}
+
+	def addForm = Action {
+		val cat = models.Category.getAll.map(c => (c.id.toString, c.name))
+		Ok(views.html.expenseAdd(expenseForm, cat))
+	}
+
+	def add = TODO
 
 }
